7b6739a25af33805de2e38fac8d96e76a4e70fe1
Deepak Kumar <dpk.41deep@gmail.com>
2024-10-01 07:51:34 +0530

    Now fixed nav bar and first content box of the website

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 // Dark mode
 const darkModeBtn = document.querySelector("#darkMode");
 darkModeBtn.addEventListener("click",(e)=>{
-    e.preventDefault();
-    const nav = document.querySelecto(".navbar");
-    nav.style = {
-        "backgroundColor":"black",
-        "color":"white"
+    // e.preventDefault();
+    const nav = document.querySelector(".navbar");
+    if(nav.style.backgroundColor!="black"){
+    nav.style.backgroundColor = "black";
+    nav.style.color = "white";
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    const searchBtn = document.querySelector(".Search");
+    searchBtn.preventDefault();
+    }else if(nav.style.backgroundColor=="black"){
+        nav.style.backgroundColor = "mintcream";
+        nav.style.color = "Black";
+        document.body.style.backgroundColor = "mintcream";
+        document.body.style.color = "Black";
     }
-})
-
-
-
-
+});
 
 
 
 //       Typing Animation
+
 const typingText = document.getElementById('typing-text');
 let text = "";
 const typingSpeed = 200;
@@ -34,7 +40,33 @@ function type() {
 
 type();
 
-//      Shortcut to go to the top of page.
-document.body.querySelector(".back_button").addEventListener("click", () => {
+    //  Shortcut to go to the top of page.
+
+    document.body.querySelector(".icons").addEventListener("click", () => {
     window.scrollTo(0, 0);
 });
+
+// const boxes = document.querySelectorAll('.box');
+// const secondTop = document.querySelector('.secondTop');
+// const height = secondTop.offsetHeight;
+// const width = secondTop.offsetWidth;
+// console.log(height);
+// console.log(width);
+
+// boxes.forEach((box) => {
+//   const randomX = Math.floor(Math.random() * (width - box.offsetWidth));
+//   const randomY = Math.floor(Math.random() * (height - box.offsetHeight));
+//   box.style.position = 'absolute';
+//   box.style.top = `${randomY}px`;
+//   box.style.left = `${randomX}px`;
+//   box.style.transition = 'all 2s ease-in-out';
+//   box.style.transform = `translate(${randomX}px, ${randomY}px)`;
+// });
+
+// setInterval(() => {
+//   boxes.forEach((box) => {
+//     const randomX = Math.floor(Math.random() * (width - box.offsetWidth));
+//     const randomY = Math.floor(Math.random() * (height - box.offsetHeight));
+//     box.style.transform = `translate(${randomX}px, ${randomY}px)`;
+//   });
+// }, 2000);
